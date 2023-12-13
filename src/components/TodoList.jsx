cb78056b846fd9c0b208a8ea9e5ebece4f8d91f3
MariangelyG <mariangelygonzalez90@gmail.com>
2023-12-13 18:07:32 -0300

    "Modifying styles"

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,37 +14,39 @@ export const TodoList = () => {
         <Box
           sx={{
             border: 5,
-            bgcolor: "#F5EBFF",
+            // bgcolor: "#F5EBFF",
             borderRadius: 3,
             textAlign: "center",
             borderColor: "secondary.main",
             width: "50%",
-            borderStyle:"double"
+            borderStyle:"double",
+           opacity: 0.7,
  
           }}
         >
-          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          <Typography variant="h3" sx={{ fontWeight: "bold" }}>
             LISTA DE TAREAS
           </Typography>
           <Typography variant="h5">
-            Todas las Tareas: {tasksItems.length}
+            <b>Todas las Tareas:</b> {tasksItems.length}
           </Typography>
           <Typography variant="h5">
-            Pendientes:{tasksItems.filter((task) => !task.done).length}{" "}
+            <b>Pendientes:</b> {tasksItems.filter((task) => !task.done).length}{" "}
           </Typography>
         </Box>
         {/* CONTENEDOR DE TABLA */}
         <Box
           sx={{
             minHeight: 250,
-            border: 2,
-            bgcolor: "#f5f5f5",
+            border: 10,
+            bgcolor: "#D59FC1",
             borderRadius: 3,
             textAlign: "center",
             borderColor: "secondary.main",
             width: "50%",
             mt: 1,
-            borderStyle:"dashed"
+            borderStyle:"groove",
+            opacity: 0.6,
           }}
         >
           {/* FORMULARIO */}
